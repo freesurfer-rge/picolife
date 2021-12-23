2a536348b3246d800d3a27dce5afe779d7acff85
R Edgar <freesurfer.rge@gmail.com>
2021-12-23 10:39:46 -0500

    Initial Commit (#1)
    Bring over code from local repo, and refactor to extract the class which drives the array.

---
+++ ledarray.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <array>
+
+#include "hardware/pio.h"
+
+#include "pioCommunicator.hpp"
+
+class LEDArray
+{
+public:
+    static constexpr float pio_freq = 50 * 1024 * 1024; // Hz
+
+    static const unsigned int nRows = 32;
+    static const unsigned int nCols = 32;
+    static const unsigned int nPixelsPerWord = 4;
+    static const unsigned int nWordsPerRow = nCols / nPixelsPerWord;
+    static const unsigned int nFrames = 16;
+
+    LEDArray(PIO targetPio);
+
+    void UpdateBuffer(
+        const std::array<uint8_t, nCols * nRows> &red,
+        const std::array<uint8_t, nCols * nRows> &green,
+        const std::array<uint8_t, nCols * nRows> &blue);
+
+    void SendBuffer();
+
+private:
+    PIOCommunicator comms;
+
+    std::array<uint32_t, (nRows / 2) * (nWordsPerRow * nFrames)> output_buffer;
+
+    bool is_pixel_on(const uint8_t value, const unsigned int iFrame);
+};
---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <set>
+#include <bitset>
+#include <array>
+#include <algorithm>
+#include <cmath>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+
+#include "led_line.pio.h"
+
+#include "pioCommunicator.hpp"
+
+#include "ledarray.hpp"
+
+
+
+uint8_t value_for_row(const unsigned int iRow)
+{
+    return iRow / 2;
+}
+
+int main()
+{
+    stdio_init_all();
+    std::cout << "LED Driver" << std::endl;
+
+    LEDArray ledArr(pio0);
+    
+    // Set up an image
+    std::array<uint8_t, 32 * 32> red;
+    std::array<uint8_t, 32 * 32> green;
+    std::array<uint8_t, 32 * 32> blue;
+    red.fill(0);
+    green.fill(0);
+    blue.fill(0);
+
+    
+    for (unsigned int iSquare = 0; iSquare < 64; iSquare++)
+    {
+        unsigned int sx = iSquare % 8;
+        unsigned int sy = iSquare / 8;
+
+        for (unsigned int ix = 0; ix < 4; ++ix)
+        {
+            for (unsigned int iy = 0; iy < 4; ++iy)
+            {
+                unsigned int idxX = sx * 4 + ix;
+                unsigned int idxY = sy * 4 + iy;
+
+                const unsigned int linearIdx = (ledArr.nCols * idxY) + idxX;
+                red.at(linearIdx) = (iSquare & 1) ? value_for_row(idxY) : 0;
+                green.at(linearIdx) = (iSquare & 2) ? value_for_row(idxY) : 0;
+                blue.at(linearIdx) = (iSquare & 4) ? value_for_row(idxY) : 0;
+            }
+        }
+    }
+    
+    
+    /*
+    for (unsigned int iy = 0; iy < ledArr.nRows; ++iy)
+    {
+        for (unsigned int ix = 0; ix < ledArr.nCols; ++ix)
+        {
+            red.at(ix + (ledArr.nCols * iy)) = iy;
+            blue[ix + (ledArr.nCols * iy)] = ((ix) <= iy) * iy;
+        }
+    }
+    */
+
+   ledArr.UpdateBuffer(red, green, blue);
+
+    std::cout << "Starting PIO" << std::endl;
+
+    unsigned long itCount = 0;
+    while (true)
+    {
+        // std::cout << "PIO running " << itCount << std::endl;
+        ledArr.SendBuffer();
+        itCount++;
+    }
+
+    return 0;
+}
