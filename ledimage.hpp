c921eeda9e7291793de0938941bacd3b1173d92a
Richard Edgar <freesurfer.rge@gmail.com>
2021-12-24 07:54:17 -0500

    Hooking things in

--- ledimage.hpp
+++ ledimage.hpp
@@ -5,12 +5,23 @@
 
 #include "ledarray.hpp"
 
+/*
+ Note that the colour channels are heap allocated so these
+ objects should be safe to stack allocate
+*/
 
-class LEDImage {
+class LEDImage
+{
 public:
     LEDImage();
 
     void Clear();
+
+    void SetPixel(const unsigned int ix, const unsigned int iy,
+                  const uint8_t r, const uint8_t g, const uint8_t b);
+
+    void SendToLEDArray(LEDArray &target) const;
+
 private:
     typedef std::array<uint8_t, LEDArray::nCols * LEDArray::nRows> Channel;
 
