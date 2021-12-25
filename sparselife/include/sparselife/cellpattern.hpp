29bb6c72f4783f6dab209475036bcab9ea1a1294
Richard Edgar <freesurfer.rge@gmail.com>
2021-12-25 15:25:16 -0500

    Starting to move sparselife to a separate library and directory

---
+++ sparselife/include/sparselife/cellpattern.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "sparselife/types.hpp"
+
+#include <istream>
+#include <memory>
+#include <set>
+
+namespace SparseLife
+{
+    class CellPattern
+    {
+    public:
+        CellPattern();
+
+        const std::set<Cell> &GetCells() const;
+
+        void LoadFromStream(std::istream &is);
+
+        void ExchangeXY();
+        void FlipX();
+        void FlipY();
+
+        void Translate(const int16_t dx, const int16_t dy);
+
+    private:
+        std::unique_ptr<std::set<Cell>> activeCells;
+
+        bool IsComment(const std::string &s) const;
+    };
+}
