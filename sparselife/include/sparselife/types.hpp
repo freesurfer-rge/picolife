a9177652b9326be9ee5fa1f660a0ee3bb1d5d6fd
R Edgar <freesurfer.rge@gmail.com>
2021-12-25 15:35:28 -0500

    Create SparseLife library (#17)
    Move the engine for Conway's Life into its own library (although still within the project).
    
    Closes #11

---
+++ sparselife/include/sparselife/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+
+namespace SparseLife
+{
+    typedef int16_t Coord_t;
+    typedef std::pair<Coord_t,Coord_t> Cell;
+}
---
+++ sparselife/src/sparselife.cpp
@@ -0,0 +1,137 @@
+#include <map>
+
+#include "sparselife/sparselife.hpp"
+
+namespace SparseLife
+{
+    SparseLife::SparseLife(const uint16_t xSize,
+                           const uint16_t ySize,
+                           const bool xWrap,
+                           const bool yWrap) : nx(xSize),
+                                               ny(ySize),
+                                               wrapX(xWrap),
+                                               wrapY(yWrap),
+                                               activeCells(std::make_unique<std::set<Cell>>())
+    {
+        // Nothing else to do
+    }
+
+    void SparseLife::AddCell(const Cell cell)
+    {
+        this->activeCells->emplace(cell);
+    }
+
+    void SparseLife::AddCells(const std::set<Cell> &cells)
+    {
+        this->activeCells->insert(cells.begin(), cells.end());
+    }
+
+    std::vector<Cell>
+    SparseLife::GetNeighbours(const Cell c) const
+    {
+        std::vector<Cell> result;
+
+        for (int16_t dx = -1; dx <= 1; ++dx)
+        {
+            int16_t xNeighbourPos = c.first + dx;
+            if (this->wrapX)
+            {
+                xNeighbourPos = this->WrapValue(xNeighbourPos, this->nx);
+            }
+            for (int16_t dy = -1; dy <= 1; ++dy)
+            {
+                int16_t yNeighbourPos = c.second + dy;
+                if (dx == 0 && dy == 0)
+                {
+                    continue; // No self-neighbouring
+                }
+
+                if (this->wrapY)
+                {
+                    yNeighbourPos = this->WrapValue(yNeighbourPos, this->ny);
+                }
+
+                // Check if neighbour is on the board
+                if (xNeighbourPos >= 0 &&
+                    xNeighbourPos < this->nx &&
+                    yNeighbourPos >= 0 &&
+                    yNeighbourPos < this->ny)
+                {
+                    result.push_back(std::make_pair(xNeighbourPos, yNeighbourPos));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    int16_t SparseLife::WrapValue(const int16_t v, const uint16_t max) const
+    {
+        if (v >= max)
+        {
+            return 0;
+        }
+        else if (v < 0)
+        {
+            return max - 1;
+        }
+        return v;
+    }
+
+    std::unique_ptr<std::set<Cell>>
+    SparseLife::ApplyRules(const std::set<Cell> &cellGrid) const
+    {
+        std::map<Cell, uint8_t> neighbourCounts;
+
+        // Have to add cells initially or lone
+        // cells can't be deleted
+        for (auto c : cellGrid)
+        {
+            neighbourCounts[c] = 0;
+        }
+
+        // Find all the neighbour counts
+        for (auto c : cellGrid)
+        {
+
+            auto neighbours = this->GetNeighbours(c);
+            for (auto n : neighbours)
+            {
+                // Recall that a C++ map will auto initialise
+                // a non-existent entry
+                neighbourCounts[n] += 1;
+            }
+        }
+
+        auto result = std::make_unique<std::set<Cell>>(cellGrid);
+
+        // Apply the rules of Life
+        for (auto nc : neighbourCounts)
+        {
+            // Kill off  things with wrong neighbour counts
+            if (nc.second < 2 || nc.second > 3)
+            {
+                // Removing a nonexistent item appears harmless...
+                result->erase(nc.first);
+            }
+
+            // Add new cells if three neighbours
+            if (nc.second == 3)
+            {
+                result->emplace(nc.first);
+            }
+        }
+
+        return result;
+    }
+
+    void SparseLife::Update()
+    {
+        this->activeCells = this->ApplyRules(*(this->activeCells));
+    }
+
+    const std::set<Cell> &SparseLife::GetCells() const
+    {
+        return *(this->activeCells);
+    }
+}
