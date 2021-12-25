528b25b5139cac7c5b3c38cdfb166869b24f796d
Richard Edgar <freesurfer.rge@gmail.com>
2021-12-25 17:00:34 -0500

    Fix up utility file

--- leddriver/src/utility.cpp
+++ leddriver/src/utility.cpp
@@ -1,23 +1,29 @@
-#include "utility.hpp"
+#include "pico/multicore.h"
+#include "pico/time.h"
 
-void core1Entry()
+#include "leddriver/utility.hpp"
+
+namespace LEDDriver
 {
-    uint32_t fifo_value = multicore_fifo_pop_blocking();
-    LEDDriver::LEDArray *ledArr = reinterpret_cast<LEDDriver::LEDArray *>(fifo_value);
-
-    const uint32_t refreshms = multicore_fifo_pop_blocking();
-
-    while (true)
+    void core1Entry()
     {
-        auto targetTime = make_timeout_time_ms(refreshms);
-        ledArr->SendBuffer();
-        sleep_until(targetTime);
-    }
-}
+        uint32_t fifo_value = multicore_fifo_pop_blocking();
+        LEDDriver::LEDArray *ledArr = reinterpret_cast<LEDDriver::LEDArray *>(fifo_value);
 
-void LaunchOnCore1(LEDDriver::LEDArray *ledArr, const uint32_t refreshms)
-{
-    multicore_launch_core1(core1Entry);
-    multicore_fifo_push_blocking(reinterpret_cast<uint32_t>(ledArr));
-    multicore_fifo_push_blocking(refreshms);
+        const uint32_t refreshms = multicore_fifo_pop_blocking();
+
+        while (true)
+        {
+            auto targetTime = make_timeout_time_ms(refreshms);
+            ledArr->SendBuffer();
+            sleep_until(targetTime);
+        }
+    }
+
+    void LaunchOnCore1(LEDDriver::LEDArray *ledArr, const uint32_t refreshms)
+    {
+        multicore_launch_core1(core1Entry);
+        multicore_fifo_push_blocking(reinterpret_cast<uint32_t>(ledArr));
+        multicore_fifo_push_blocking(refreshms);
+    }
 }
